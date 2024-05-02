7111c9fc4057850c353e5843f9a963fe94a38107
Alejandra <168584883+alegarciap@users.noreply.github.com>
2024-05-01 20:58:01 -0600

    Update MovimientoGranel.java

--- MovimientoGranel.java
+++ MovimientoGranel.java
@@ -1,24 +1,32 @@
 package objetosNegocio;
 
 /**
- * Clase que representa un movimiento de un producto a granel (comprado o vendido).
- * 
+ * Clase que representa un movimiento de un producto a granel (comprado o
+ * vendido). Hereda de la clase Movimiento.
+ *
  * @author Alejandra García Preciado
  */
 public class MovimientoGranel extends Movimiento {
-    
+
     // Atributos 
-    private Movimiento tipoMov; // compra o venta
+    /**
+     * El tipo de movimiento (compra o venta).
+     */
+    private Movimiento tipoMov;
+
+    /**
+     * El producto a granel asociado al movimiento.
+     */
     private ProductoGranel productoGranel;
-    
+
     // Constructores
     /**
      * Constructor por defecto que inicializa el producto a granel en null.
      */
-    public MovimientoGranel(){
+    public MovimientoGranel() {
         this.productoGranel = null;
     }
-    
+
     /**
      * Constructor que inicializa el movimiento con un producto a granel dado.
      *
@@ -27,53 +35,65 @@ public class MovimientoGranel extends Movimiento {
     public MovimientoGranel(ProductoGranel productoGranel) {
         this.productoGranel = productoGranel;
     }
-    
+
     /**
      * Constructor que inicializa el movimiento con una clave de movimiento.
-     * 
-     * @param cveMovimiento La clave del movimiento.
+     *
+     * @param claveMov La clave del movimiento.
      */
-    public MovimientoGranel(String claveMov){
+    public MovimientoGranel(String claveMov) {
+        super(claveMov);
         this.tipoMov = null;
     }
-    
+
     // Métodos de acceso
-    
+    /**
+     * Obtiene el tipo de movimiento asociado al movimiento de granel.
+     *
+     * @return El tipo de movimiento asociado al movimiento de granel.
+     */
     public Movimiento getTipoMov() {
         return tipoMov;
     }
-    
+
+    /**
+     * Establece el tipo de movimiento asociado al movimiento de granel.
+     *
+     * @param tipoMov El nuevo tipo de movimiento asociado al movimiento de
+     * granel.
+     */
     public void setTipoMov(Movimiento tipoMov) {
         this.tipoMov = tipoMov;
     }
-    
+
     /**
      * Obtiene el producto a granel asociado al movimiento.
-     * 
+     *
      * @return El producto a granel asociado al movimiento.
      */
-    public ProductoGranel getProductoGranel(){
+    public ProductoGranel getProductoGranel() {
         return productoGranel;
     }
-    
+
     /**
      * Establece el producto a granel asociado al movimiento.
-     * 
+     *
      * @param productoGranel El nuevo producto a granel asociado al movimiento.
      */
-    public void setProductoGranel(ProductoGranel productoGranel){
+    public void setProductoGranel(ProductoGranel productoGranel) {
         this.productoGranel = productoGranel;
     }
-    
+
     // Método toString
     /**
      * Devuelve una representación en forma de cadena de este MovimientoGranel.
-     * 
-     * @return Una cadena que contiene la representación en cadena del producto a granel asociado.
+     *
+     * @return Una cadena que contiene la representación en cadena del producto
+     * a granel asociado.
      */
     @Override
-    public String toString(){
+    public String toString() {
         return productoGranel.toString();
     }
-    
+
 }
