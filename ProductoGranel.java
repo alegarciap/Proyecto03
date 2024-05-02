fcd66e4d8d58e68bc59728331fdb89167a1c7bb7
Alejandra <168584883+alegarciap@users.noreply.github.com>
2024-05-01 20:57:16 -0600

    Update ProductoGranel.java

--- ProductoGranel.java
+++ ProductoGranel.java
@@ -1,19 +1,23 @@
 package objetosNegocio;
 
 /**
- * Clase que representa un producto a granel, que es un tipo de producto.
- * Hereda de la clase Producto.
- * 
+ * Clase que representa un producto a granel, que es un tipo de producto. Hereda
+ * de la clase Producto.
+ *
  * @author Alejandra García Preciado
  */
 public class ProductoGranel extends Producto {
-    
+
     // Atributos
+    /**
+     * La cantidad del producto a granel.
+     */
     private float cantidad;
-    
+
     // Constructores
     /**
      * Constructor que inicializa los atributos con los valores proporcionados.
+     *
      * @param clave La clave del producto a granel.
      * @param nombre El nombre del producto a granel.
      * @param tipo El tipo del producto a granel.
@@ -24,24 +28,35 @@ public class ProductoGranel extends Producto {
         super(clave, nombre, tipo, unidad);
         this.cantidad = cantidad;
     }
-    
+
+    /**
+     * Obtiene la cantidad del producto a granel.
+     *
+     * @return La cantidad del producto a granel.
+     */
     public float getCantidad() {
         return cantidad;
     }
-    
+
+    /**
+     * Establece la cantidad del producto a granel.
+     *
+     * @param cantidad La nueva cantidad del producto a granel.
+     */
     public void setCantidad(float cantidad) {
         this.cantidad = cantidad;
     }
-    
+
     // Método toString
     /**
      * Devuelve una representación en forma de cadena de este ProductoGranel.
-     * 
-     * @return Una cadena que contiene los valores de los atributos del ProductoGranel.
+     *
+     * @return Una cadena que contiene los valores de los atributos del
+     * ProductoGranel.
      */
     @Override
-    public String toString(){
+    public String toString() {
         return super.toString() + "," + cantidad;
     }
-    
+
 }
